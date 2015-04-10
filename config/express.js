2315eaa8e2143cbc65a7c4243059179b949bfb76
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-10 16:16:58 +0100

    Project Init

---
+++ config/express.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+module.exports = function() {
+  var app = express();
+
+  if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(methodOverride());
+
+  require('../app/routes/user-routes')(app);
+
+  return app;
+};
