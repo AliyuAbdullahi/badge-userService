206ce4ceecb1ed45e2a908785ff3a4b2d7eba84a
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-13 23:27:31 +0100

    Fix the controller bug

--- app/features/controllers/userServiceController.js
+++ app/features/controllers/userServiceController.js
@@ -1,33 +1,36 @@
 var User = require('../../../config/ptgres')()[0];
-
-exports.createUser = function(req,res){
-  new User(req.body).save().then(function(model){
+//var User = require('../model/userModel')[0];
+module.exports={
+createUser : function(req,res){
+  console.log('crete');
+  User.forge(req.body).save().then(function(model){
     if(model){
       res.json(model);
     }
   });
-};
-exports.getAlluser = function(req,res){
-  new User().fetchAll().then(function(data){
+},
+getAlluser : function(req,res){
+  User().fetchAll().then(function(data){
   if(data){
     res.json(data);
   }
 });
-};
-exports.getUserByName = function(req, res){
+},
+getUserByName : function(req, res){
   new User({username:username}).then(function(data){
    if(data){
     res.json(data);
    }
   });
-};
-exports.removeUser = function(req, res){
+},
+removeUser: function(req, res){
   User({"username": req.body.username}).destroy().then(function(){
     res.json("User has been destroyed");
   });
 
-};
-exports.putUser = function(req, res){
+},
+putUser: function(req, res){
   User().set(req.params.body);
+}
 };
     
