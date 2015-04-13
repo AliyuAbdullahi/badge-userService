a021b0d09cc9cc230c5101c7fa8058c065110f2e
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-14 00:07:29 +0100

    Test user model

--- app/tests/model-spec.js
+++ app/tests/model-spec.js
@@ -1,41 +1,41 @@
-// var User = require('../../config/postgres')()[0],
-//     db = require('../../config/postgres')()[1],
-//     user;
+var User = require('../../config/ptgres')()[0],
+    db = require('../../config/ptgres')()[1],
+    user;
 
-// describe('userModel Test', function() {
+describe('userModel Test', function() {
     
-//     beforeEach(function(done) {
-//       user = new User({
-//         username: "bello",
-//         password: "fadilat",
-//         firstname: "opeyemi",
-//         lastname: "taiwo",
-//         is_admin: false,
-//         auth: "4444"
-//       });
-//       done();
-//     });
+    beforeEach(function(done) {
+      user = new User({
+        username: "bello",
+        password: "fadilat",
+        firstname: "opeyemi",
+        lastname: "taiwo",
+        is_admin: false,
+        auth: "4444"
+      });
+      done();
+    });
 
-//     afterEach(function(done) {
-//         db.knex('users')
-//           .where('username', "eniola")
-//           .del().then(function() {
-//              console.log("deleted");
-//           });
-//         done();
-//     });
+    afterEach(function(done) {
+        db.knex('users')
+          .where('username', "bello")
+          .del().then(function() {
+             console.log("DataBase has been created successfully");
+          });
+        done();
+    });
 
-//     describe("Test if the new row is saved", function() {
-//       it("should be able to save a data without issues", function(done) {
-//             user.save().then(function(model) {
-//                 expect(model.attributes).toEqual(jasmine.objectContaining({
-//                     "username": "bello",
-//                     "password": "fadilat"
-//                 }));
-//                  done();
-//             });
-//       });
-//     });
-// });
+    describe("Save row", function() {
+      it("should be able to save table row without issues", function(done) {
+            user.save().then(function(model) {
+                expect(model.attributes).toEqual(jasmine.objectContaining({
+                    "username": "bello",
+                    "password": "fadilat"
+                }));
+                 done();
+            });
+      });
+    });
+});
     
 
