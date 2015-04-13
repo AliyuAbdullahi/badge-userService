0b0901d79e996df99c872c455297d14193ec7710
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-14 00:06:45 +0100

    Add get request

--- app/features/routes/userServiceRoute.js
+++ app/features/routes/userServiceRoute.js
@@ -1,7 +1,7 @@
 var controllerData = require('../controllers/userServiceController');
 module.exports = function(app) {
-  app.route('/users').post(controllerData.createUser);
-  //app.route('/users').get(controllerData.getAllUser);
+  app.route('/users').post(controllerData.createUser).get(controllerData.getAlluser);
+  // app.route('/users').get(controllerData.getAllUser);
   // router.route('/users/:userId')
   // .get(controllerData.getUserByName).put(controllerData.update).delete(controllerData.delete);
 };
