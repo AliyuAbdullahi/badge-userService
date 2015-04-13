20c2f510d9d0242ab40b01fde0cfffcd26df9b1c
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-13 23:31:51 +0100

    clean up the server code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var db = require("./config/ptgres");
+var db = require("./config/ptgres")();
 
 
 //var app = express();
-var config = require('./config/env/config');
-var app = require('./config/express')(db);
+var config = require('./config/env/config')();
+var app = require('./config/express')();
 app.listen(config.port, function () {
     console.log('Express Servicce  app listening on port: ' + config.port);
 });
