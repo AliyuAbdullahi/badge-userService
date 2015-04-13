1c9323b4b5eef071e6e13dee5bbc7acc65181252
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-13 23:31:02 +0100

    fix conflict error on the postgres database

--- config/ptgres.js
+++ config/ptgres.js
@@ -1,14 +1,15 @@
- var config = require('../config/env/config'),
-    knex = require('knex');
+ var config = require('./env/config')(),
+    knex = require('knex'),
+    anything = require('../app/features/models/userModel');
 
 module.exports = function() {
   var db = knex({
         client: 'pg',
-        connection: config.db  
+        connection: config.db.connection 
   });
-    return db;
+   var shit = anything(db); 
   //   var User = require('../app/models/userModel')(db);
 
-  // return User;
+  return shit;
 
 };
