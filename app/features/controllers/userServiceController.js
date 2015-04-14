5d0d9a0511d885392b9e9b4b39b3b1d7074daee7
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-14 21:33:30 +0100

    Add json web token

--- app/features/controllers/userServiceController.js
+++ app/features/controllers/userServiceController.js
@@ -1,14 +1,33 @@
 var User = require('../../../config/ptgres')()[0];
+jwt = require('jsonwebtoken'),
+//secret = require('../../../config/env/config').secret,
 //var User = require('../model/userModel')[0];
 module.exports={
 createUser : function(req,res){
-  console.log('create');
-  User.forge(req.body).save().then(function(model){
-    if(model){
-      res.json(model);
-    }
-  });
+  if(req.body.username && req.body.password){
+     var profile = {
+                username: req.body.username,
+                email: req.body.email
+            };
+            var secret = 'TOPSECRETTTTT';
+            var token = jwt.sign(profile, secret);
+            User.forging({
+              username: req.body.firstname,
+                password: req.body.lastname,
+                firstname: req.body.username,
+                lastname: req.body.password,
+                is_admin: false,
+                auth: token
+            }).save().then(function(method) {
+                   res.json({success: "User created",
+                             token: method.attributes.auth});
+                });      
+
+  }else{
+     res.json({error: "You need to Register by filling the field"});
+  }
 },
+
 getAlluser : function(req,res){
   new User().fetchAll().then(function(data){
   if(data){
@@ -16,28 +35,39 @@ getAlluser : function(req,res){
   }
 });
 },
-getOneUser:function(req,res){
-  new User().fetch().then(function(collection) {
-  collection.at(0)
-  .load(['username', 'firstname', 'lastname'])
-  .then(function(model) {
-    JSON.stringify(model);
-  });
+
+updateOneUser:function(req,res){
+ new User({'username': req.body.oldname}).fetch().then(function(data){
+  if(data){
+    delete req.body.oldname;
+    data.save(req.body, {patch: true}).then(function(){
+      res.json({success: "User data saved successfully"});
+    });
+  }
+  else{
+    res.json({error: "Error saving file"});
+  }
 });
 },
+
 getUserByName : function(req, res){
    new User({'username':req.params.username}).fetch().then(function(data){
     res.json(data);
   });
 },
+
 removeUser: function(req, res){
-  User({username: req.body.username}).destroy().then(function(){
+ new User({username: req.body.username}).fetch().then(function(data){
+    if(data){
+      data.destroy().save().then(function(){
+        res.json({Success: "File deleted"});
+      });
+    }
     res.json("User has been destroyed");
   });
-
 },
 putUser: function(req, res){
-  User().set(req.params.body);
-}
+    User().set(req.params.body);
+  }
 };
     
