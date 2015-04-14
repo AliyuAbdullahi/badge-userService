4c9d3309431011659ee6d72efe3998ae2626c79f
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-14 21:35:20 +0100

    npm install json-web-token

--- app/features/routes/userServiceRoute.js
+++ app/features/routes/userServiceRoute.js
@@ -2,5 +2,6 @@ var controllerData = require('../controllers/userServiceController');
 module.exports = function(app) {
   app.route('/users').post(controllerData.createUser).get(controllerData.getAlluser);
   app.route('/users/:username').get(controllerData.getUserByName);
+  app.route('/users/edit').put(controllerData.updateOneUser);
 
 };
