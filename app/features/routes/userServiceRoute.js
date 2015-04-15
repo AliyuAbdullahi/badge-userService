ede711569365b6e0f768f57eb233a81cf711e58b
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-16 00:26:36 +0100

    add delete feature

--- app/features/routes/userServiceRoute.js
+++ app/features/routes/userServiceRoute.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
   app.route('/users/:username').get(controllerData.getUserByName);
   app.route('/users/signout').post(controllerData.Usersignout);
   app.route('/users/edit').put(controllerData.updateOneUser);
-  app.route('users/:username').delete(controllerData.removeUser);
+  app.route('/users/delete').delete(controllerData.UserDelete);
+  // app.route('users/delete').delete(controllerData.UserDelete);
 
 };  
